d280e285c63c7136315bf48a3ef56cba60f0f9e8
Alex Maybaum <amaybaum@gmail.com>
2017-04-13 17:27:46 +0300

    add recursive dependency tree resolution method, and bump to version 0.5

--- src/main/groovy/org/whitesource/gradle/tasks/CollectProjectInfoTask.groovy
+++ src/main/groovy/org/whitesource/gradle/tasks/CollectProjectInfoTask.groovy
@@ -1,6 +1,10 @@
 package org.whitesource.gradle.tasks
 
 import org.gradle.api.DefaultTask
+import org.gradle.api.Project
+import org.gradle.api.artifacts.ResolvedDependency
+import org.gradle.api.internal.artifacts.ivyservice.resolveengine.ModuleConflictResolver
+import org.gradle.api.internal.project.DefaultProject
 import org.gradle.api.tasks.TaskAction
 import org.whitesource.agent.api.ChecksumUtils
 import org.whitesource.agent.api.model.AgentProjectInfo
@@ -14,6 +18,8 @@ import org.whitesource.gradle.WhitesourceConfiguration
 class CollectProjectInfoTask extends DefaultTask {
 
     WhitesourceConfiguration wssConfig = project.whitesource
+
+    static addedSha1s = new HashSet<String>()
 
     @TaskAction
     def CollectProjectInfos() {
@@ -33,27 +39,27 @@ class CollectProjectInfoTask extends DefaultTask {
             thisProjConf.name in wssConfig.includedConfigurationNames || thisProjConf.name in wssConfig.includedConfigurations*.name
         }
 
-        def addedSha1s = new HashSet<String>()
-        configurationsToInclude*.resolvedConfiguration*.getFiles(wssConfig.dependencyFilter).flatten().each {
-            def sha1 = ChecksumUtils.calculateSHA1(it)
-            if (!addedSha1s.contains(sha1)) {
-                def dependencyInfo = new DependencyInfo()
-                // TODO populate actual artifactId
-                dependencyInfo.setArtifactId(it.name)
-                // TODO populate groupId and version
-                dependencyInfo.setGroupId(it.name)
-                dependencyInfo.setVersion(it.name)
-                dependencyInfo.setSystemPath(it.absolutePath)
-                dependencyInfo.setSha1(sha1)
-
-                // TODO collect transitive dependencies
-                dependencyInfo.getChildren().add()
-
-                projectInfo.getDependencies().add(dependencyInfo)
-                addedSha1s.add(sha1)
-            }
+        configurationsToInclude*.resolvedConfiguration*.getFirstLevelModuleDependencies(wssConfig.dependencyFilter).flatten().each { dependency ->
+            def info = getDependencyInfo(dependency as ResolvedDependency)
+            projectInfo.getDependencies().add(info)
         }
 
         project.projectInfos.add(projectInfo)
+    }
+
+    def getDependencyInfo(ResolvedDependency dependency) {
+        def dependencyInfo = new DependencyInfo()
+        def sha1 = ChecksumUtils.calculateSHA1(dependency.moduleArtifacts[0].file)
+        if (!addedSha1s.contains(sha1)) {
+            dependencyInfo.setGroupId(dependency.getModuleGroup())
+            dependencyInfo.setArtifactId(dependency.getModuleName())
+            dependencyInfo.setVersion(dependency.getModuleVersion())
+            dependencyInfo.setSha1(sha1)
+            addedSha1s.add(sha1)
+        }
+        dependency.getChildren().each {
+            dependencyInfo.getChildren().add(getDependencyInfo(it))
+        }
+        return dependencyInfo
     }
 }
